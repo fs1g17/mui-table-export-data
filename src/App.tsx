e723e42eeb40c9e969810cec8ef802bec3dcff06
Fedor Selenskiy <47851444+fs1g17@users.noreply.github.com>
2023-04-15 00:02:53 +0100

    Working Demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,64 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ExportTable, { UserData } from './components/ExportTable';
+
+const heroes: UserData[] = [
+  {
+    userId: 1,
+    username: "Captain Chuckles",
+    userEmail: "captain.chuckles@superheromail.com",
+  },
+  {
+    userId: 2,
+    username: "The Giggle Guru",
+    userEmail: "giggle.guru@superheromail.com",
+  },
+  {
+    userId: 3,
+    username: "Mirthful Mantis",
+    userEmail: "mirthful.mantis@superheromail.com",
+  },
+  {
+    userId: 4,
+    username: "Doctor Puns",
+    userEmail: "doctor.puns@superheromail.com",
+  },
+  {
+    userId: 5,
+    username: "Snicker Siren",
+    userEmail: "snicker.siren@superheromail.com",
+  },
+  {
+    userId: 6,
+    username: "LOL Llama",
+    userEmail: "lol.llama@superheromail.com",
+  },
+  {
+    userId: 7,
+    username: "Smirk Spectre",
+    userEmail: "smirk.spectre@superheromail.com",
+  },
+  {
+    userId: 8,
+    username: "Grin Guardian",
+    userEmail: "grin.guardian@superheromail.com",
+  },
+  {
+    userId: 9,
+    username: "Chortle Cheetah",
+    userEmail: "chortle.cheetah@superheromail.com",
+  },
+  {
+    userId: 10,
+    username: "The Hilarity Hero",
+    userEmail: "hilarity.hero@superheromail.com",
+  },
+];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ExportTable userData={heroes} />
     </div>
   );
 }
